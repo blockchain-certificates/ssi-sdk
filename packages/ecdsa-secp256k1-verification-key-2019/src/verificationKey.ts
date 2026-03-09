210b097c3e21fcca225467732fdfed9540920837
Julien Fraichot <julien.fraichot@hyland.com>
2026-03-09 18:06:32 +0100

    fix(Sign): keep ISO behavior with previous scep256k1 package when signing

--- packages/ecdsa-secp256k1-verification-key-2019/src/verificationKey.ts
+++ packages/ecdsa-secp256k1-verification-key-2019/src/verificationKey.ts
@@ -167,7 +167,7 @@ export class EcdsaSecp256k1VerificationKey2019 extends cryptoLd.LDKeyPair {
           ),
         )
 
-        const { signature } = await secp256k1.signAsync(digest, base58.decode(privateKeyBase58))
+        const signature = await secp256k1.signAsync(digest, base58.decode(privateKeyBase58), { prehash: false })
         const encodedSignature = base64url.encode(Buffer.from(signature))
 
         return `${encodedHeader}..${encodedSignature}`
